bb2f18eff64fdd68d1f1b5e1fb44d88d86805846
Dachi301 <qirikashvili2020@gmail.com>
2023-01-26 14:54:37 +0400

    fix: images on card and added animation on button

--- src/components/cards/card.tsx
+++ src/components/cards/card.tsx
@@ -3,25 +3,23 @@ import { component$ } from '@builder.io/qwik'
 interface PropsTypes {
   title: string
   body: string
+  imgSrc: string
 }
 
-const Card = component$(({ title, body }: PropsTypes) => {
+const Card = component$(({ title, body, imgSrc }: PropsTypes) => {
   return (
     <div
       class={
         'flex w-full flex-col gap-[10px] rounded-[10px] border border-[black] p-[10px]'
       }
     >
-      <img
-        class={'h-[550px] w-full'}
-        src='https://w0.peakpx.com/wallpaper/203/1010/HD-wallpaper-forest-flora-forest-vithurshan-dark-europe-faded-green-leaf-mood-moody-sri-lanka-vithurshan-jpeg-wood.jpg'
-      />
+      <img class={'aspect-[3/2.5] h-full object-cover'} src={imgSrc} />
       <h1 class={'text-center'}>{title}</h1>
       <p class={'font text-[12px] font-[200]'}>{body}</p>
       <div class={'flex items-center justify-center'}>
         <button
           class={
-            'w-[30%] rounded-[20px] bg-[#00b3ff] py-[5px] text-[2vh] text-black transition-[0.3s] hover:bg-[#0066ff] hover:text-white'
+            'w-[30%] translate-y-0 rounded-[20px] bg-[#e7c128] py-[5px] text-[2vh] text-black transition-all hover:bg-[#000] hover:text-[#e7c128] active:translate-y-1'
           }
         >
           View
--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,10 +1,72 @@
-import { component$ } from '@builder.io/qwik'
+import { component$, useClientEffect$ } from '@builder.io/qwik'
 import type { DocumentHead } from '@builder.io/qwik-city'
 import Card from '~/components/cards/card'
 import Header from '~/components/header/header'
 import { APP_TITLE } from '~/utils/constants'
 
+// Image imports
+import Ps5Img from '../images/ps5.jpg'
+import NikeImg from '../images/nike.jpg'
+import NatureImg from '../images/nature.jpg'
+import GoatImg from '../images/goat.jpg'
+
+interface CardProps {
+  title: string
+  body: string
+  imgSrc: string
+}
+
 export default component$(() => {
+  const Items: CardProps = [
+    {
+      title: 'PS5',
+      body: 'Lorem ipsum dolor, sit amet consectetur adipisicing elit',
+      imgSrc: `${Ps5Img}`,
+    },
+    {
+      title: 'Nike',
+      body: 'Lorem ipsum dolor, sit amet consectetur adipisicing elit',
+      imgSrc: `${NikeImg}`,
+    },
+    {
+      title: 'Nature',
+      body: 'Lorem ipsum dolor, sit amet consectetur adipisicing elit',
+      imgSrc: `${NatureImg}`,
+    },
+    {
+      title: 'Messi',
+      body: 'Lorem ipsum dolor, sit amet consectetur adipisicing elit',
+      imgSrc: `${GoatImg}`,
+    },
+    {
+      title: 'Lorem2',
+      body: 'Lorem ipsum dolor, sit amet consectetur adipisicing elit',
+      imgSrc: `${NatureImg}`,
+    },
+    {
+      title: 'Jquery + PHP > React + Laravel',
+      body: 'Lorem ipsum dolor, sit amet consectetur adipisicing elit',
+      imgSrc: `${NatureImg}`,
+    },
+    {
+      title: 'Qwik',
+      body: 'Lorem ipsum dolor, sit amet consectetur adipisicing elit',
+      imgSrc: `${NatureImg}`,
+    },
+    {
+      title: 'Node.JS',
+      body: 'Lorem ipsum dolor, sit amet consectetur adipisicing elit',
+      imgSrc: `${NatureImg}`,
+    },
+  ]
+
+  useClientEffect$(() => {
+    Items.map((item) => {
+      // Just for test
+      console.log(item)
+    })
+  })
+
   return (
     <div class={'w-full'}>
       <Header />
@@ -28,11 +90,14 @@ export default component$(() => {
         <div
           class={'grid w-full grid-cols-4 gap-x-[30px] gap-y-[30px] px-[30px]'}
         >
-          <Card title='text1' body='loremdoasfoasdkfo' />
-          <Card title='text2' body='lorem2' />
-          <Card title='text3' body='lorem3' />
-          <Card title='text4' body='lorem4' />
-          <Card title='text5' body='lorem5' />
+          {Array.isArray(Items) &&
+            Items.map((item: any) => (
+              <Card
+                title={item.title}
+                body={item.body}
+                imgSrc={`${item.imgSrc}`}
+              />
+            ))}
         </div>
       </div>
     </div>
