58a984701ee55d9a5dc74050d892b7e4fce50625
Dachi301 <qirikashvili2020@gmail.com>
2023-01-26 15:33:42 +0400

    style: make shorten text on cards

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -11,6 +11,7 @@ import NatureImg from '../images/nature.jpg'
 import GoatImg from '../images/goat.jpg'
 
 interface CardProps {
+  id: number
   title: string
   body: string
   imgSrc: string
@@ -19,56 +20,65 @@ interface CardProps {
 export default component$(() => {
   const Items: CardProps = [
     {
+      id: new Date().valueOf(),
       title: 'PS5',
-      body: 'Lorem ipsum dolor, sit amet consectetur adipisicing elit',
+      body: 'Lorem ipsum dolor, sit amet elit asdfasfg',
       imgSrc: `${Ps5Img}`,
     },
     {
+      id: new Date().valueOf(),
       title: 'Nike',
-      body: 'Lorem ipsum dolor, sit amet consectetur adipisicing elit',
+      body: 'Lorem ipsum dolor, sit amet consectetur adipisicing elit fsgregqer t',
       imgSrc: `${NikeImg}`,
     },
     {
+      id: new Date().valueOf(),
       title: 'Nature',
-      body: 'Lorem ipsum dolor, sit amet consectetur adipisicing elit',
+      body: 'Lorem ipsum dolor, sit amet consectetur adipisicing elittr rtq qwrqwer qewr qwewr',
       imgSrc: `${NatureImg}`,
     },
     {
+      id: new Date().valueOf(),
       title: 'Messi',
-      body: 'Lorem ipsum dolor, sit amet consectetur adipisicing elit',
+      body: 'Lorem ipsum dolor, sit amet consectetur adipisicing elit yyyt5 hnbret asFSDsd asd as',
       imgSrc: `${GoatImg}`,
     },
     {
+      id: new Date().valueOf(),
       title: 'Lorem2',
-      body: 'Lorem ipsum dolor, sit amet consectetur adipisicing elit',
+      body: 'Lorem ipsum dolor, sit amet consectetur adipisicing elit Lorem ipsum dolor, sit amet consectetur adipisicing elitLorem ipsum dolor, sit amet consectetur adipisicing elit',
       imgSrc: `${NatureImg}`,
     },
     {
+      id: new Date().valueOf(),
       title: 'Jquery + PHP > React + Laravel',
-      body: 'Lorem ipsum dolor, sit amet consectetur adipisicing elit',
+      body: 'Lorem ipsum dolor, sit amet consectetur adipisicing elit g gtew gtehyh r trewr rewt',
       imgSrc: `${NatureImg}`,
     },
     {
+      id: new Date().valueOf(),
       title: 'Qwik',
-      body: 'Lorem ipsum dolor, sit amet consectetur adipisicing elit',
+      body: 'Lorem ipsum dolor, sit amet consectetur adipisicing elit t ywetyewtytewytew tr tewrtewrt r',
       imgSrc: `${NatureImg}`,
     },
     {
+      id: new Date().valueOf(),
       title: 'Node.JS',
-      body: 'Lorem ipsum dolor, sit amet consectetur adipisicing elit',
+      body: 'Lorem ipsum dolor, sit amet consectetur adipisicing elit oqeworiqwoeriqwoeroqiw trqtretertre re',
       imgSrc: `${NatureImg}`,
     },
   ]
 
-  useClientEffect$(() => {
-    Items.map((item) => {
-      // Just for test
-      console.log(item)
-    })
-  })
+  function truncate(str: string, n: number) {
+    let shortText = str.length > n ? str.substr(0, n - 1) + '...' : str
+    if (shortText.length < n) {
+      return str.substr(0, n - 1) + '...'
+    }
+    return shortText
+  }
 
   return (
-    <div class={'w-full'}>
+    <div class={'mb-[20px] w-full'}>
       <Header />
       <div
         class={
@@ -94,7 +104,7 @@ export default component$(() => {
             Items.map((item: any) => (
               <Card
                 title={item.title}
-                body={item.body}
+                body={truncate(item.body, 75)}
                 imgSrc={`${item.imgSrc}`}
               />
             ))}
---
+++ src/routes/item/[itemId]/index.tsx
@@ -0,0 +1,9 @@
+import { component$ } from '@builder.io/qwik'
+
+export default component$(() => {
+  return (
+    <div class='min-h-screen bg-blue-50'>
+      <h1>Item PAGEEEEE</h1>
+    </div>
+  )
+})
