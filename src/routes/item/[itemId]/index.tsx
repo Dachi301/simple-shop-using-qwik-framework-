123f1cd9b6fd7c17b46067868a22b3e1fe92f9ce
Dachi301 <qirikashvili2020@gmail.com>
2023-02-01 14:19:48 +0400

    added some spaces on cart page

--- src/routes/item/[itemId]/index.tsx
+++ src/routes/item/[itemId]/index.tsx
@@ -35,40 +35,42 @@ export default component$(() => {
   return (
     <div>
       <div class={'px-[30px]'}>
-        <div class={'flex gap-[100px]'}>
-          <img src={item[0]?.imgSrc} class={'h-[500px] object-cover'} />
-          <div class={'flex flex-col'}>
-            <h1 class={'text-[7em]'}>{item[0].title}</h1>
-            <div class={'pl-[10px]'}>
-              <div class={'flex flex-col gap-[15px]'}>
-                <p class={'text-[22px]'}>ფასი: {state.itemPrice} ₾</p>
-                <div class={'flex items-center gap-[20px]'}>
-                  <button
-                    class={
-                      'rounded-[10px] bg-[gray] py-[10px] px-[20px] text-[25px] outline-0'
-                    }
-                    onClick$={handleIncrement$}
-                  >
-                    +
-                  </button>
-                  <p>{state.count}</p>
-                  <button
-                    class={
-                      'rounded-[10px] bg-[gray] py-[10px] px-[20px] text-[25px] outline-0'
-                    }
-                    onClick$={handleDecrement$}
-                  >
-                    -
-                  </button>
-                </div>
-                <div>
-                  <button
-                    class={
-                      'mt-[20px] flex translate-y-0 justify-start rounded-[10px] bg-[#e7c128] py-[20px] px-[20px] text-[20px] transition-all active:translate-y-1'
-                    }
-                  >
-                    დამატება კალათაში
-                  </button>
+        <div class={'relative flex h-full w-full gap-[100px]'}>
+          <div class={'mt-[50px] mb-[50px] flex gap-[100px]'}>
+            <img src={item[0]?.imgSrc} class={'h-[500px] object-cover'} />
+            <div class={'flex flex-col'}>
+              <h1 class={'text-[7em]'}>{item[0].title}</h1>
+              <div class={'pl-[10px]'}>
+                <div class={'flex flex-col gap-[15px]'}>
+                  <p class={'text-[22px]'}>ფასი: {state.itemPrice} ₾</p>
+                  <div class={'flex items-center gap-[20px]'}>
+                    <button
+                      class={
+                        'rounded-[10px] bg-[gray] py-[10px] px-[20px] text-[25px] outline-0'
+                      }
+                      onClick$={handleIncrement$}
+                    >
+                      +
+                    </button>
+                    <p>{state.count}</p>
+                    <button
+                      class={
+                        'rounded-[10px] bg-[gray] py-[10px] px-[20px] text-[25px] outline-0'
+                      }
+                      onClick$={handleDecrement$}
+                    >
+                      -
+                    </button>
+                  </div>
+                  <div>
+                    <button
+                      class={
+                        'mt-[20px] flex translate-y-0 justify-start rounded-[10px] bg-[#e7c128] py-[20px] px-[20px] text-[20px] transition-all active:translate-y-1'
+                      }
+                    >
+                      დამატება კალათაში
+                    </button>
+                  </div>
                 </div>
               </div>
             </div>
