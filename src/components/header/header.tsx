c3f2eb362559aea616676cb30ce425e0a3c186ea
Dachi301 <qirikashvili2020@gmail.com>
2023-02-01 14:22:46 +0400

    added navigation to cart page

--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -15,7 +15,12 @@ const Header = component$(() => {
           Simple Shop System
         </p>
         <div class={'relative'}>
-          <p class='cursor-pointer text-[2rem] text-white'>Cart</p>
+          <p
+            onClick$={() => (nav.path = '/cart')}
+            class='cursor-pointer text-[2rem] text-white'
+          >
+            Cart
+          </p>
           <div
             class={
               'absolute top-0 left-[-15px] flex h-[20px] w-[20px] cursor-pointer items-center justify-center rounded-[50%] bg-black text-[14px] text-white'
