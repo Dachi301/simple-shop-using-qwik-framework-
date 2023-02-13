2fe5028dd3f7eb5b0da9ec51fcf9317ecb2451cf
Dachi301 <qirikashvili2020@gmail.com>
2023-02-13 14:05:29 +0400

    chore changes

--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -1,6 +1,7 @@
-import { component$, useContext } from '@builder.io/qwik'
+import { component$, useContext, useStore } from '@builder.io/qwik'
 import { useNavigate } from '@builder.io/qwik-city'
 import { CartContext } from '~/context/Cart'
+import AuthModal from '../modals/auth-modal'
 
 const Header = component$(() => {
   const nav = useNavigate()
@@ -18,22 +19,26 @@ const Header = component$(() => {
         >
           Simple Shop System
         </p>
-        <div class={'relative'}>
-          <p
-            onClick$={() => {
-              nav.path = '/cart'
-            }}
-            class='cursor-pointer text-[2rem] text-white'
-          >
-            Cart
-          </p>
-          <div
-            class={
-              'absolute top-0 left-[-15px] flex h-[20px] w-[20px] cursor-pointer items-center justify-center rounded-[50%] bg-black text-[14px] text-white'
-            }
-          >
-            {/* {state} */}
-            {ctxState.cartLength}
+        <div class={'flex items-center gap-[40px]'}>
+          {' '}
+          <p class='cursor-pointer text-[2rem] text-white'>Login</p>
+          <div class={'relative'}>
+            <p
+              onClick$={() => {
+                nav.path = '/cart'
+              }}
+              class='cursor-pointer text-[2rem] text-white'
+            >
+              Cart
+            </p>
+            <div
+              class={
+                'absolute top-0 left-[-15px] flex h-[20px] w-[20px] cursor-pointer items-center justify-center rounded-[50%] bg-black text-[14px] text-white'
+              }
+            >
+              {/* {state} */}
+              {ctxState.cartLength}
+            </div>
           </div>
         </div>
       </header>
