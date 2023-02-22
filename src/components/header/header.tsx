1d1978d1927e4b0b9250a81a5fb5c93147d9cfd0
Dachi301 <qirikashvili2020@gmail.com>
2023-02-22 13:38:13 +0400

    style: added responsive counter on a cart icon

--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -47,7 +47,12 @@ const Header = component$(() => {
             </div>
           </div>
           <div class={'hidden gap-[20px] md:flex'}>
-            <div>
+            <div
+              class={'relative'}
+              onClick$={() => {
+                nav.path = '/cart'
+              }}
+            >
               <svg
                 xmlns='http://www.w3.org/2000/svg'
                 fill='none'
@@ -55,9 +60,6 @@ const Header = component$(() => {
                 strokeWidth={1.5}
                 stroke='white'
                 className='hidden h-6 w-6 md:flex'
-                onClick$={() => {
-                  nav.path = '/cart'
-                }}
               >
                 <path
                   strokeLinecap='round'
@@ -65,6 +67,14 @@ const Header = component$(() => {
                   d='M2.25 3h1.386c.51 0 .955.343 1.087.835l.383 1.437M7.5 14.25a3 3 0 00-3 3h15.75m-12.75-3h11.218c1.121-2.3 2.1-4.684 2.924-7.138a60.114 60.114 0 00-16.536-1.84M7.5 14.25L5.106 5.272M6 20.25a.75.75 0 11-1.5 0 .75.75 0 011.5 0zm12.75 0a.75.75 0 11-1.5 0 .75.75 0 011.5 0z'
                 />
               </svg>
+              <div
+                class={
+                  'absolute top-[-10px] left-[-15px] hidden h-[20px] w-[20px] cursor-pointer items-center justify-center rounded-[50%] bg-black text-[14px] text-white md:flex'
+                }
+              >
+                {/* {state} */}
+                {ctxState.cartLength}
+              </div>
             </div>
             <svg
               xmlns='http://www.w3.org/2000/svg'
